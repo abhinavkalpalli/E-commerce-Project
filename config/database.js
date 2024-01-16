ee0ed244df945fae117caf3c2c22793b9aa5fdd8
abhinavkalpalli <abhinav.kbaburaj@gmail.com>
2024-01-16 11:08:34 +0530

    complete

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 
 module.exports= connection=>{
     const databaseURL=process.env.DATABASE_URL;
-
+    console.log(databaseURL)
     mongoose.connect(databaseURL).then(()=>{
+        
         console.log('Database connected successfully...')
     }).catch(err =>{
         console.log(err.message);
-        return res.redirect('/500')
     })
 }
